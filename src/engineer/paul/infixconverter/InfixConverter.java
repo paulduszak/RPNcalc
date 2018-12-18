7d5d23c73361e1e22f5ec7f99260c2b7cdf21f79
Paul Duszak <duszak.paul@gmail.com>
2018-12-18 16:51:38 -0600

    Add InfixConverter and InfixConverterTest classes
    InfixConverter will provide static methods to convert infix expressions
    to PostFix and PreFix expressions.

---
+++ src/engineer/paul/infixconverter/InfixConverter.java
@@ -0,0 +1,31 @@
+package engineer.paul.infixconverter;
+
+public class InfixConverter {
+
+    /**
+     * Returns a Postfix expression
+     *
+     * @param infixExpression an expression where the operator comes between operands
+     * @return an expression where operators come after corresponding operands
+     */
+    public static String toPostFix(String infixExpression) {
+
+        // TODO: Implement toPostFix function
+
+        return null;
+    }
+
+    /**
+     * Returns a Prefix expression
+     *
+     * @param infixExpression an expression where the operator comes between operands
+     * @return an expression where operators come after corresponding operands
+     */
+    public static String toPreFix(String infixExpression) {
+
+        // TODO: Implement toPreFix function
+
+        return null;
+    }
+
+}
---
+++ tests/engineer/paul/infixconverter/InfixConverterTest.java
@@ -0,0 +1,22 @@
+package engineer.paul.infixconverter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InfixConverterTest {
+
+    @Test
+    void toPostFix() {
+
+        // TODO: Implement toPostFix test case
+
+    }
+
+    @Test
+    void toPreFix() {
+
+        // TODO: Implment toPreFix test case
+
+    }
+}
